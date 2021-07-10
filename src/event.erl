cb8d95e5d6708ed9ee6c047e0ff7ea22d5a43bd7
Colby Rabideau <me@colbyr.com>
2021-07-10 15:16:39 -0400

    finish the event module

--- src/event.erl
+++ src/event.erl
@@ -4,12 +4,6 @@
 
 -record(state, {server, name = "", to_go = 0}).
 
-%% Because Erlang is limited to about 49 days (49*24*60*60*1000) in
-%% milliseconds, the following function is used
-normalize(N) ->
-  Limit = 9 * 24 * 60 * 60,
-  [N rem Limit | lists:duplicate(N div Limit, Limit)].
-
 loop(S = #state{server = Server, to_go = [T | Next]}) ->
   receive
     {Server, Ref, cancel} ->
@@ -28,11 +22,25 @@ start(EventName, Delay) ->
 start_link(EventName, Delay) ->
   spawn_link(?MODULE, init, [self(), EventName, Delay]).
 
+%% Because Erlang is limited to about 49 days (49*24*60*60*1000) in
+%% milliseconds, the following function is used
+normalize(N) ->
+  Limit = 9 * 24 * 60 * 60,
+  [N rem Limit | lists:duplicate(N div Limit, Limit)].
+
+time_to_go(TimeOut = {{_, _, _}, {_, _, _}}) ->
+  Now = calendar:local_time(),
+  ToGo =
+    calendar:datetime_to_gregorian_seconds(TimeOut)
+    - calendar:datetime_to_gregorian_seconds(Now),
+  Seconds = max(ToGo, 0),
+  normalize(Seconds).
+
 %%% Event's innards
-init(Server, EventName, Delay) ->
+init(Server, EventName, DateTime) ->
   loop(#state{server = Server,
               name = EventName,
-              to_go = normalize(Delay)}).
+              to_go = time_to_go(DateTime)}).
 
 cancel(Pid) ->
   Ref = erlang:monitor(process, Pid),
