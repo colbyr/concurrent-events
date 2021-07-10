d9d5fec3376b8df402375b40a936feff6e3584c6
Colby Rabideau <me@colbyr.com>
2021-07-10 15:55:11 -0400

    event_serv interfaces

--- src/event_serv.erl
+++ src/event_serv.erl
@@ -85,3 +85,54 @@ loop(S = #state{}) ->
 
 init() ->
   loop(#state{events = orddict:new(), clients = orddict:new()}).
+
+start() ->
+  register(?MODULE, Pid = spawn(?MODULE, init, [])),
+  Pid.
+
+start_link() ->
+  register(?MODULE, Pid = spawn_link(?MODULE, init, [])),
+  Pid.
+
+terminate() ->
+  ?MODULE ! shutdown.
+
+add_event(Name, Description, TimeOut) ->
+  Ref = make_ref(),
+  ?MODULE ! {self(), Ref, {add, Name, Description, TimeOut}},
+  receive
+    {Ref, Msg} ->
+      Msg
+  after 5000 ->
+    {error, timeout}
+  end.
+
+cancel(Name) ->
+  Ref = make_ref(),
+  ?MODULE ! {self(), Ref, {cancel, Name}},
+  receive
+    {Ref, ok} ->
+      ok
+  after 5000 ->
+    {error, timeout}
+  end.
+
+listen(Delay) ->
+  receive
+    M = {done, _Name, _Description} ->
+      [M | listen(0)]
+  after Delay * 1000 ->
+    []
+  end.
+
+subscribe(Pid) ->
+  Ref = erlang:monitor(process, whereis(?MODULE)),
+  ?MODULE ! {self(), Ref, {subscribe, Pid}},
+  receive
+    {Ref, ok} ->
+      {ok, Ref};
+    {'DOWN', Ref, process, _Pid, Reason} ->
+      {error, Reason}
+  after 5000 ->
+    {error, timeout}
+  end.
